ee9e413fce9a408d3189c71caf9d950631a754bf
uiuuiu <duydyy@gmail.com>
2018-07-06 14:03:02 +0700

    limit home service postswq

--- app/models/service_category.rb
+++ app/models/service_category.rb
@@ -1,6 +1,7 @@
 class ServiceCategory < ApplicationRecord
 
   has_many :service_posts
+  has_many :home_service_posts, -> {order(created_at: :desc).limit(5)}, class_name: ServicePost.name, foreign_key: :service_category_id
 
   def self.all_without_temp
     where.not(name: "temp")
